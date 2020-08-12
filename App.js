aa78eb926d3adda36948c410a1140813f36b2fcf
JuanYut <juanyut97@hotmail.com>
2020-08-12 17:57:46 -0500

    My Initial with Plus One Func

--- App.js
+++ App.js
@@ -1,21 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
+const Logo = () => <Text>Title Component</Text>;
+
+// * Home -----------------------------------------------------------------
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home!</Text>
+      <Button
+        title="Go to Details"
+        // onPress={() => navigation.push("Details")}
+        onPress={() =>
+          navigation.navigate("Details", { title: "User 1", user_id: 1 })
+        }
+      />
     </View>
   );
-}
+};
+
+// HomeScreen.navigationOptions = ({ navigation }) => {
+//   return {
+//     title: navigation.getParam("title", "Cargando..."),
+//   };
+// };
+HomeScreen.navigationOptions = {
+  headerTitle: <Logo />,
+  headerStyle: {
+    backgroundColor: "#ffeecc",
+  },
+  headerTintColor: "#222",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
+
+// * Details ---------------------------------------------------------------
+const DetailsScreen = ({ navigation }) => {
+  const [cont, setCont] = useState(0);
+  const plusOne = () => setCont(cont + 1);
+
+  useEffect(() => {
+    navigation.setParams({ plusOne });
+  }, [cont]);
+
+  const lala = navigation.getParam("lala", "default value :c");
+  return (
+    <View style={styles.container}>
+      <Text>Details! {cont}</Text>
+      {/* <Button title="Go Back" onPress={() => navigation.goBack()} /> */}
+      <Button
+        title="Go Back"
+        onPress={() => navigation.setParams({ title: "User 1" })}
+      />
+    </View>
+  );
+};
+
+DetailsScreen.navigationOptions = ({ navigation, navigationOptions }) => {
+  return {
+    title: navigation.getParam("title", "Cargando..."),
+    headerRight: (
+      <Button
+        onPress={navigation.getParam("plusOne")}
+        title="Plus 1"
+        color="#222"
+      />
+    ),
+    headerStyle: {
+      backgroundColor: navigationOptions.headerStyle.backgroundColor,
+    },
+  };
+};
+
+/*
+  Componente de navegacion
+  recibe un obj de configuracion, el cual seran las pantallas de navegacion.
+*/
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#222",
+      },
+      headerTintColor: "#ff5f40",
+      headerTitleStyle: {
+        fontWeight: "900",
+      },
+    },
+  }
+);
+
+/*
+  createAppContainer creara un componente que va a manejar todo el estado de navegacion de la aplicacion.
+  Recibe un componente que tiene que ser de navegacion.
+*/
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+## Navigation React Native
+
+👻 A fun project with React Native
+
+- **Contributors:**
+
+  - Juan Santillán a.k.a. JuanYut
